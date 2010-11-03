15ceda446b0d7643234669409057944eefb36bd4
Robert Schönthal <caziel@gmx.net>
2010-11-03 22:35:34 +0100

    initial plugin structure

---
+++ config/rsCssSpritesPluginConfiguration.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * rsCssSpritesPlugin configuration.
+ * 
+ * @package     rsCssSpritesPlugin
+ * @subpackage  config
+ * @author      Your name here
+ * @version     SVN: $Id: PluginConfiguration.class.php 17207 2009-04-10 15:36:26Z Kris.Wallsmith $
+ */
+class rsCssSpritesPluginConfiguration extends sfPluginConfiguration
+{
+  const VERSION = '1.0.0-DEV';
+
+  /**
+   * @see sfPluginConfiguration
+   */
+  public function initialize()
+  {
+  }
+}
---
+++ lib/task/generateSpritesTask.class.php
@@ -0,0 +1,41 @@
+<?php
+
+class generateSpritesTask extends sfBaseTask
+{
+  protected function configure()
+  {
+    // // add your own arguments here
+    // $this->addArguments(array(
+    //   new sfCommandArgument('my_arg', sfCommandArgument::REQUIRED, 'My argument'),
+    // ));
+
+    // // add your own options here
+    // $this->addOptions(array(
+    //   new sfCommandOption('my_option', null, sfCommandOption::PARAMETER_REQUIRED, 'My option'),
+    // ));
+
+    $this->namespace        = 'generate';
+    $this->name             = 'sprites';
+    $this->briefDescription = 'generates css sprites';
+    $this->detailedDescription = <<<EOF
+The [generate:sprites|INFO] task generates css sprites for you.
+See http://csssprites.org/ how to use it.
+Call it with:
+
+  [php symfony generate:sprites|INFO]
+EOF;
+  }
+
+  protected function execute($arguments = array(), $options = array())
+  {
+    $params[] = "--root-dir-path ".sfConfig::get("sf_web_dir")."/css";
+    $params[] = "--sprite-png-ie6";
+    $params[] = "--document-root-dir-path ".sfConfig::get("sf_web_dir");
+
+    $script = dirname(__FILE__)."/../vendor/smartsprites/smartsprites.sh ";
+
+    $command = $script.join(" ", $params);
+
+    $this->log($this->getFilesystem()->execute($command));
+  }
+}
